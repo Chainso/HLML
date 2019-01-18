827bfa469119d133392bf6a9e755c9b44c437ac5
Chainso <chanzobryan@gmail.com>
2019-01-17 21:51:53 -0500

    Fixed more GAN stuff

--- PyTorch/SL/GAN/gan.py
+++ PyTorch/SL/GAN/gan.py
@@ -30,35 +30,44 @@ class GAN(GANModel):
         if(self.writer is None):
             self.writer = SummaryWriter(logs_path)
 
-    def train_batch(self, data):
+    def train_batch(self, data, disc_steps):
         """
         Trains the model for the given batch of data
 
         data : The data to train the model on
+        disc_steps : The number of discriminator steps to train for per
+                     per generator
         """
-        gen_data = self.gen(data)
-        gen_data_detach = self.gen(data).detach()
-
-        ones_target = torch.ones(data.shape[0], device = self.device)
-        zeros_target = torch.zeros(data.shape[0], device = self.device)
+        ones_target = torch.ones(data.shape[0], 1, device = self.device)
+        zeros_target = torch.zeros(data.shape[0], 1, device = self.device)
 
         loss_func = nn.BCELoss()
 
-        disc_on_gen = self.disc(gen_data).view(-1)
-        disc_on_gen_detach = self.disc(gen_data_detach).view(-1)
-        disc_on_real = self.disc(data).view(-1)
+        disc_fake_loss = 0
+        disc_real_loss = 0
+        disc_loss = 0
 
-        disc_fake_loss = loss_func(disc_on_gen_detach, zeros_target)
-        disc_real_loss = loss_func(disc_on_real, ones_target)
-        disc_loss = disc_fake_loss + disc_real_loss
-
-        gen_loss = loss_func(disc_on_gen, ones_target)
-
-        self.disc_optim.zero_grad()
-        disc_loss.backward()
-        self.disc_optim.step()
+        for _ in range(disc_steps):
+            self.disc_optim.zero_grad()
+            gen_data_detach = self.gen(data).detach()
+    
+            disc_on_gen_detach = self.disc(gen_data_detach)
+            disc_on_real = self.disc(data)
+    
+            disc_fake_loss = loss_func(disc_on_gen_detach, zeros_target)
+            disc_real_loss = loss_func(disc_on_real, ones_target)
+            disc_loss = disc_fake_loss + disc_real_loss
+    
+            disc_loss.backward()
+            self.disc_optim.step()
 
         self.gen_optim.zero_grad()
+
+        gen_data = self.gen(data)
+
+        disc_on_gen = self.disc(gen_data)
+        gen_loss = loss_func(disc_on_gen, ones_target)
+
         gen_loss.backward()
         self.gen_optim.step()
 
@@ -70,6 +79,10 @@ class GAN(GANModel):
         if(self.writer is not None):
             self.writer.add_scalar("Generator Loss", np_gen_loss,
                                    self.num_batches)
+            self.writer.add_scalar("Discriminator Real Loss",
+                                   disc_real_loss.item(), self.num_batches)
+            self.writer.add_scalar("Discriminator Fake Loss",
+                                   disc_fake_loss.item(), self.num_batches)
             self.writer.add_scalar("Discriminator Loss", np_disc_loss,
                                    self.num_batches)
 
